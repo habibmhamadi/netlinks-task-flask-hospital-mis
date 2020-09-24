1eb8435fa432e68d127a697e21b006f47bc2d039
Habib Mhamadi <habibmhamadi@gmail.com>
2020-09-24 11:45:19 +0430

    Authentication implemented

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, session, redirect, request, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import secrets
+import base64
 
 app = Flask(__name__)
 app.secret_key = secrets.token_urlsafe(16)
@@ -17,69 +18,105 @@ from models.department import Department
 from models.auth import Auth
 
 
-def authenticate(request_url):
-    if 'email' in session:
-        return render_template(request_url)
-    return redirect('login')
+def authenticate():
+    return 'fullname' in session
     
-def guest(request_url):
-    if 'email' in session:
-        return redirect('/')
-    return render_template(request_url)
-    
-
 @app.route("/")
 def home():
-    return authenticate('index.html')
+    if authenticate():
+        apps = Appointment.query.all()
+        deps = Department.query.all()
+        pats = Patient.query.all()
+        docs = Doctor.query.all()
+        return render_template('index.html',items=[
+            len(apps),
+            len(deps),
+            len(pats),
+            len(docs)
+        ])
+    return redirect('/login')
 
 @app.route("/appointments")
 def appointments():
-    return authenticate('appointments.html')
+    if authenticate():
+        apps = Appointment.query.all()
+        return render_template('appointment/index.html',apps=apps)
+    return redirect('/login')
 
 @app.route("/departments")
 def departments():
-    return authenticate('departments.html')
+    if authenticate():
+        deps = Appointment.query.all()
+        return render_template('department/index.html',deps=deps)
+    return redirect('/login')
 
 @app.route("/patients")
 def patients():
-    return authenticate('patients.html')
+    if authenticate():
+        pats = Appointment.query.all()
+        return render_template('patient/index.html',pats=pats)
+    return redirect('/login')
 
 @app.route("/doctors")
 def doctors():
-    return authenticate('doctors.html')
+    if authenticate():
+        docs = Appointment.query.all()
+        return render_template('doctor/index.html',docs=docs)
+    return redirect('/login')
 
 @app.route("/login", methods=['POST','GET'])
 def login():
+    if authenticate():
+        return redirect('/')
     if request.method =='POST':
         eml = request.form['email']
         pwd = request.form['password']
-        a = Auth.query.filter_by(email=eml).filter_by(password=pwd).first()
-        return 'None' if a==None else Auth.serialize(a)
-    #     if request.form['email'] == 'habib@gmail.com':
-    #         session['email'] = request.form['email']
-    #         return redirect('/')
-    #     else:
-    #         flash('Incorrect credentials')
-    #         return redirect('/login')
-    elif request.method == 'GET':
-        return guest('auth/login.html')
-    # return 'Invalid route method'
+        pwd = base64.b64encode(pwd.encode("utf-8"))
+        res = Auth.query.filter_by(email=eml).filter_by(password=str(pwd)).first()
+        if res is None:
+            flash('Incorrect credentials')
+            return redirect('/login')
+        else:
+            session['fullname'] = res.fullname
+            return redirect('/')
+    return render_template('auth/login.html')
 
 @app.route("/register", methods=['GET','POST'])
 def register():
+    if authenticate():
+        return redirect('/')
     if request.method =='POST':
-        session['email'] = request.form['email']
-        return redirect('/')
-    elif request.method == 'GET':
-        return guest('auth/register.html')
-    return 'Invalid route method'
+        fname = request.form['fullname']
+        eml = request.form['email']
+        pwd = request.form['password']
+        pwd = base64.b64encode(pwd.encode("utf-8"))
+        res = Auth.query.filter_by(email=eml).first()
+        if res is None:
+            newAuth = Auth(
+                fullname = fname,
+                email = eml,
+                password = str(pwd)
+            )
+            db.session.add(newAuth)
+            db.session.commit()
+            session['fullname'] = fname
+            return redirect('/')
+        else: 
+            flash('Email has already taken!')
+            return redirect('/register')
+    return render_template('auth/register.html')
 
-@app.route("/logout", methods=['GET'])
+@app.route("/logout")
 def logout():
-    if request.method =='GET':
-        session.pop('email', None)
-        return redirect('/')
-    return 'Invalid route method'
+    if authenticate():
+        session.pop('fullname', None)
+        return redirect('/login')
+    return render_template('/login')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def page_not_found(path):
+    return render_template('404.html')
 
     
 # if __name__ == "__main__":
